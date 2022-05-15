ab08791de6c0eb167490475150d98dae92eec280
Rick Casillas <casillasrick@yahoo.com>
2022-05-14 23:02:17 -0400

    Attempting to get the function right

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  alert("Password Must Be Between 8 and 20 Characters")
+  alert("Password Must Be Between 8 and 128 Characters")
   confirm("Passwords may contain lowercase, uppercase, numbers, and special characters")
-
-
-return "stay tuned";
+  
+  return password;
 
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password").value;
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@()*#@&-";
+  var passwordLength = 15;
+
+
+  for(var i = 0; i < passwordLength; i++){
+    var index = math.floor(math.random() * chars.passwordLength);
+    passwordText = chars[index];
+  }
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
